42f3fc7512e6e35509103db7b0cccc0a1593e2ad
Samux <samuferfort@gmail.com>
2025-03-19 23:51:58 +0100

    add react compiler

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["pdf-parse"],
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import SignOut from "@/components/auth/sign-out";
 import { redirect } from "next/navigation";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
+
 export default async function Home() {
   const session = await auth.api.getSession({
     headers: await headers(),
